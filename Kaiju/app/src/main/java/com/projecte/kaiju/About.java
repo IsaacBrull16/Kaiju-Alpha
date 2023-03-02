a1bd38de67b18c5d494049f5a2ef892798d49bd2
LordVader03 <pelliterogarciaalex@gmail.com>
2023-03-02 16:47:41 +0400

    about done

--- Kaiju/app/src/main/java/com/projecte/kaiju/About.java
+++ Kaiju/app/src/main/java/com/projecte/kaiju/About.java
@@ -2,7 +2,9 @@ package com.projecte.kaiju;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button b = findViewById(R.id.homebutton);
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- Kaiju/app/src/main/java/com/projecte/kaiju/MainActivity.java
+++ Kaiju/app/src/main/java/com/projecte/kaiju/MainActivity.java
@@ -2,6 +2,7 @@ package com.projecte.kaiju;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         termsId = (EditText) findViewById(R.id.termsId);
         acceptTerms = (Button) findViewById(R.id.acceptTerms);
 
+        Button a = findViewById(R.id.aboutbutton);
+        Intent i = new Intent(this, About.class);
+        startActivity(i);
+        finish();
     }
     public void invisible(View v){
 
