e467622da70cee81abdf1c01d8f41895dce9c4dd
paula <paulasilland@gmail.com>
2023-03-24 17:12:12 +0100

    login

---
+++ Kaiju/app/src/main/java/com/projecte/kaiju/activities/Login.java
@@ -0,0 +1,16 @@
+package com.projecte.kaiju.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projecte.kaiju.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
---
+++ Kaiju/app/src/main/java/com/projecte/kaiju/activities/SignUp.java
@@ -0,0 +1,16 @@
+package com.projecte.kaiju.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projecte.kaiju.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
