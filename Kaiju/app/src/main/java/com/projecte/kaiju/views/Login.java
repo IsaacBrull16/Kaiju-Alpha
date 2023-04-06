1dac01ebe6c190d7a64f6615b640d6f0715978fd
César <cesar.alejandro98@hotmail.com>
2023-04-06 13:10:40 +0200

    hola buenas noches me llamo cesar por si no me conoces

--- Kaiju/app/src/main/java/com/projecte/kaiju/views/Login.java
+++ Kaiju/app/src/main/java/com/projecte/kaiju/views/Login.java
@@ -1,4 +1,4 @@
-package com.projecte.kaiju.views;
+/*package com.projecte.kaiju.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +12,98 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+    }
+}*/
+package com.projecte.kaiju.views;
+
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+
+        import com.projecte.kaiju.R;
+
+public class Login extends AppCompatActivity {
+
+    protected String myClassTag = this.getClass().getSimpleName();
+
+    EditText etName;
+    EditText etPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+        findViewById(R.id.btLogin).setOnClickListener(v -> login());
+        findViewById(R.id.btSignUp).setOnClickListener(v -> loginToSignUp());
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void loginToSignUp() {
+        Intent i = new Intent(this, SignUp.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        reload();
+    }
+
+
+    private void login() {
+        String email = etName.getText().toString();
+        String password = etPassword.getText().toString();
+
+        ActivityHelper.hideKeyboard(this);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(myClassTag, "signInWithEmail:success");
+                            reload();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(myClassTag, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            etPassword.setText("");
+                        }
+                    }
+                });
+    }
+
+    private void reload(){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            if (currentUser.isEmailVerified()){
+                etPassword.setText("");
+                etEmail.setText("");
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+            }else{
+                Toast.makeText(LoginActivity.this, R.string.VerifyEmail,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
