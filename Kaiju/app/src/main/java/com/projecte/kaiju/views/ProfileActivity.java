6499aa0b5869780822ad10a6980aaf4e1c1b8cc2
LordVader03 <pelliterogarcialex@gmail.com>
2023-04-18 14:21:14 +0200

    UsrName getter and setter almost Done

--- Kaiju/app/src/main/java/com/projecte/kaiju/views/ProfileActivity.java
+++ Kaiju/app/src/main/java/com/projecte/kaiju/views/ProfileActivity.java
@@ -1,22 +1,30 @@
 package com.projecte.kaiju.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.projecte.kaiju.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.projecte.kaiju.helpers.GlobalInfo;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase db;
-    private DatabaseReference myRef;
+    private DatabaseReference usrRef;
+    private String name;
 
     EditText userNameP;
     EditText userEmail;
@@ -28,13 +36,29 @@ public class ProfileActivity extends AppCompatActivity {
         userNameP = findViewById(R.id.userNameP);
         userEmail = findViewById(R.id.userEmail);
 
+        String url = GlobalInfo.getInstance().getFB_DB();
+
         mAuth = FirebaseAuth.getInstance();
-        db = FirebaseDatabase.getInstance();
+        db = FirebaseDatabase.getInstance(url);
 
-        FirebaseUser user = mAuth.getCurrentUser();
-        userEmail.setText(user.getEmail());
+        FirebaseUser usr = mAuth.getCurrentUser();
+        userEmail.setText(usr.getEmail());
+        String id = usr.getUid();
 
-        myRef = db.getReference();
+        usrRef = db.getReference("users").child(id);
+        usrRef.child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                name = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        userNameP.setText(name);
+        System.out.println("\n\n\n\n" + name);
 
         findViewById(R.id.logOutButton).setOnClickListener(v -> logOut());
         findViewById(R.id.homeButtonP).setOnClickListener(v -> home());
@@ -65,6 +89,12 @@ public class ProfileActivity extends AppCompatActivity {
 
     public void rename(){
         String newname = userNameP.getText().toString();
+        usrRef.child("name").setValue(newname).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+
+            }
+        });
     }
 
     public void changePswd(){
