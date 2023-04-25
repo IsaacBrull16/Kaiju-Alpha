61f8d7a29890ccec5c6c7f4cc2eb30c27170a67a
LordVader03 <pelliterogarciaalex@gmail.com>
2023-04-25 17:31:24 +0900

    CSV Reader Done

--- Kaiju/app/src/main/java/com/projecte/kaiju/models/Deck.java
+++ Kaiju/app/src/main/java/com/projecte/kaiju/models/Deck.java
@@ -1,6 +1,5 @@
 package com.projecte.kaiju.models;
 
-import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -11,17 +10,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.opencsv.CSVReader;
 import com.projecte.kaiju.helpers.CardHelper;
 import com.projecte.kaiju.helpers.GlobalInfo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.projecte.kaiju.helpers.CardsFactoryHelper;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -123,46 +115,8 @@ public class Deck {
     }
 
     public void allCards() {
-        for (int i = 0; i < rowsData; i++){
-                int id = Integer.parseInt(data[i][0]);
-                String name = data[i][1];
-                int cost = Integer.parseInt(data[i][2]);
-                int damage = Integer.parseInt(data[i][3]);
-                int life = Integer.parseInt(data[i][4]);
-                String type = data[i][5];
-                totalCards.add(new Card(id, name, cost, damage, life, type));
-        }
-        /*try {
-            String csvFile = "kaiju.csv";
-            CSVReader csvReader = new CSVReader(new FileReader(csvFile));
-            //FileReader fr = new FileReader(csvFile);
-            //BufferedReader br = new BufferedReader(fr);
-            String[] line = null;
-            while ((line = csvReader.readNext()) != null) {
-                //String[] values = line.split(",");
-                int id = Integer.parseInt(line[0]);
-                String name = line[1];
-                int cost = Integer.parseInt(line[2]);
-                int damage = Integer.parseInt(line[3]);
-                int life = Integer.parseInt(line[4]);
-                String type = line[5];
-                totalCards.add(new Card(id, name, cost, damage, life, type));
-            }
-            csvReader.close();
-            //fis.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }*/
+        totalCards = CardsFactoryHelper.readData();
     }
-
-    private static final String[][] data = {{"0","Trotowild","1","2","5","Nature"},
-        {"1","PlantBot","2","6","2","Technology"},
-        {"2","ElectroRazz","1","2","4","Science"},
-        {"3","TechnoLight","5","10","2","Technology"},
-        {"4","DuckWind","4","2","9","Nature"},
-        {"5","PlasticKiller","6","11","3","Science"}};
-
-    private int rowsData = data.length;
 
     public void compareDecks(){
         for (int i = 0; i < personalDeck.size(); i++){
